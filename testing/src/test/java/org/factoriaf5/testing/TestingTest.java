a949e153096293154003ce9f5c19cb0e6ddc3e46
andreamsgi27 <andreamsgi27@hotmail.com>
2024-10-21 17:14:51 +0200

    improved

--- testing/src/test/java/org/factoriaf5/testing/TestingTest.java
+++ testing/src/test/java/org/factoriaf5/testing/TestingTest.java
@@ -1,6 +1,7 @@
 package org.factoriaf5.testing;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,13 +11,21 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestingTest {
+    private Testing testing;
+
+    @BeforeEach
+    public void init(){
+        this.testing = new Testing();
+    }
+
+
     @Test
     public void testSumar() {
-        //Example example =  new Example(); crear el objecto
-        Testing testing = new Testing();
         // Given - Teniendo:
         int num1 = 35; // parametros
         int num2 = 79;
@@ -35,22 +44,25 @@ public class TestingTest {
     @Test
     public void testCheckPositivo() {
         //crear el objecto
-        Testing testing = new Testing();
-        int num1 = 5;
-        int num2 = -5;
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            testing.checkPositivo(num2);
-        });
+        int num = 5;
+        
         // When - Cuando
-        boolean result = testing.checkPositivo(num1);
+        boolean result = testing.checkPositivo(num);
         // Then - Entonces
-        assertEquals(true, result);
-        assertTrue(num1 > 0);
-        assertFalse(num1 < 0);
+        assertEquals(true, num > 0);
+        assertTrue(num > 0);
+        assertFalse(num < 0);
         assertNotNull(result);
         //assertEquals("El número no puede ser negativo", result2);
-        assertEquals("El número no puede ser negativo", exception.getMessage());
+        
     }
+    @Test
+    public void testCheckPositivoError() {
+        //crear el objecto
+        int num = -5;
+        assertThrows(IllegalArgumentException.class, () -> {testing.checkPositivo(num);});
+    }
+
 
     @Test
     public void testContarLetrasA() {
@@ -71,97 +83,131 @@ public class TestingTest {
     }
 
     @Test
+    public void testContarLetrasNoA() {
+        String  cadena = "gehnlk";
+        int result = testing.contarLetrasA(cadena);
+        // Then - Entonces
+        assertEquals(0,  result);
+    }
+
+    @Test
+    public void testContarLetrasAisEmpty() {
+        String cadena = "";
+        int result = testing.contarLetrasA(cadena);
+        assertEquals(0, result);
+    }
+
+
+
+    @Test
     public void testContieneElemento() {
         //crear el objecto
-        Testing testing = new Testing();
         
         String elemento = "hola";
-        String elemento2 = "jeje";
         List<String> lista = List.of(new String[]{"hola", "mundo", "java"});
         
 
         // When - Cuando
         boolean result = testing.contieneElemento(lista, elemento);
-        boolean result2 = testing.contieneElemento(lista, elemento2);
 
         // Then - Entonces
-        assertEquals(true, result);
-        assertFalse(result2);
-        assertNotNull(result);
         assertTrue(result);
-        assertTrue(result == true);
-        assertFalse(result == false);
+    }
+    @Test
+    public void testContieneElementoNo() {
+        //crear el objecto
+        
+        String elemento = "adios";
+        List<String> lista = List.of(new String[]{"hola", "mundo", "java"});
+        
+
+        // When - Cuando
+        boolean result = testing.contieneElemento(lista, elemento);
+
+        // Then - Entonces
+        assertFalse(result);
+    }
+    @Test
+    public void testContieneElementoVacio() {
+        String elemento = "adios";
+        List<String> lista = Collections.emptyList();
+        // When - Cuando
+        boolean result = testing.contieneElemento(lista, elemento);
+        // Then - Entonces
+        assertFalse(result);
     }
 
     @Test
     public void testRevertirCadena() {
-        //crear el objecto
-        Testing testing = new Testing();
-        
-        
         String elemento1 = "hola pato";
         
-
         // When - Cuando
         String result = testing.revertirCadena(elemento1);
 
         // Then - Entonces
         assertEquals("otap aloh", result);
         assertNotNull(result);
-        assertTrue(result.equals("otap aloh"));
         assertFalse(result.equals("pato hola"));
-
     }
 
     @Test
     public void testFactorial() {
-        //crear el objecto
-        Testing testing = new Testing();
-        
-        
         int num = 4;
-        int num2 = -4;
         
 
         // When - Cuando
         long result = testing.factorial(num);
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            testing.factorial(-5);});
 
         // Then - Entonces
         assertEquals(24, result);
-        assertFalse(num < 0);
+    }
+    @Test
+    public void testFactorialNegativo() {
+        int num = -4;
+        
+        // When - Cuando
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            testing.factorial(num);});
+
+        // Then - Entonces
         assertEquals("Factorial no definido para números negativos", exception.getMessage());
     }
 
     @Test
     public void testEsPrimo() {
-        //crear el objecto
-        Testing testing = new Testing();
-        
-        int num = 7;
         int num2 = 12;
-        int num3 = 0;
 
         // When - Cuando
-        boolean result = testing.esPrimo(num);
         boolean result2 =  testing.esPrimo(num2);
+
+        // Then - Entonces
+        assertFalse(result2);
+    }
+
+    @Test
+    public void testEsPrimoMenorIguala1() {
+        
+        int num3 = 0;
         boolean result3 =  testing.esPrimo(num3);
 
         // Then - Entonces
-        assertEquals(true, result);
-        assertFalse(result2);
-        assertNotNull(result);
-        assertTrue(result == true);
-        assertFalse(result == false);
         assertFalse(result3);
     }
+
+    @Test
+    public void testEsPrimoMayorACuatro() {
+        
+        int num = 7;
+        boolean result =  testing.esPrimo(num);
+
+        // Then - Entonces
+        assertTrue(result);
+    }
+
     
     @Test
     public void testMensajeConRetraso() throws InterruptedException {
         //daba error todo el tiempo porque era poniendo el throws arriba
-        //crear el objecto
-        Testing testing = new Testing();
         
         // When - Cuando
         String result = testing.mensajeConRetraso();
@@ -172,8 +218,6 @@ public class TestingTest {
     
     @Test
     public void testConvertirAString(){
-        //crear el objecto
-        Testing testing = new Testing();
 
         // Crear una lista de ints para la prueba
         List<Integer> lista = new ArrayList<>();
@@ -201,8 +245,6 @@ public class TestingTest {
 
     @Test
     public void testCalcularMedia() {
-        // crear el objeto
-        Testing testing = new Testing();
 
         List<Integer> lista = new ArrayList<>();
         lista.add(1);
@@ -229,35 +271,42 @@ public class TestingTest {
     }
 
     @Test
-    public void testConvertirListaAString(){
-        //crear el objecto
-        Testing testing = new Testing();
+    public void testCalcularMediaMal() {
+    }
 
-        List<String> lista = new ArrayList<>();
-        lista.add("uno");
-        lista.add("dos");
-        lista.add("tres");
+    @Test
+    public void testConvertirListaAStringUnNull(){
 
         List<String> lista2 = new ArrayList<>();
         lista2.add("uno");
         lista2.add(null);
         lista2.add("tres");
 
+        String result2 = testing.convertirListaAString(lista2);
+        assertEquals("UNO,NULL,TRES", result2);
+    }
+
+    @Test
+    public void testConvertirListaAStringNula(){
+
         List<String> lista3 = new ArrayList<>();
         lista3.add(null);
         lista3.add(null);
         lista3.add(null);
 
-        String result = testing.convertirListaAString(lista);
-        String result2 = testing.convertirListaAString(lista2);
         String result3 = testing.convertirListaAString(lista3);
 
-        assertEquals("UNO,DOS,TRES", result);
-        assertFalse(false, "TRES");
-        assertTrue(true, "UNO,DOS,TRES");
-        assertNotNull(result);
-        assertEquals("UNO,NULL,TRES", result2);
         assertEquals("NULL,NULL,NULL", result3);
+    }
+
+    @Test
+    public void testConvertirListaAStringVacia(){
+
+        List<String> lista = new ArrayList<>();
+
+        String result = testing.convertirListaAString(lista);
+
+        assertEquals("", result);
     }
 
 }
